e1088c1c8bcb83aa9f6f50472794db0985298441
Keshav Rathor <35619632+keshav955@users.noreply.github.com>
2021-06-28 10:12:03 +0530

    User Book Isuuing added

--- LibraryManagement/Startup.cs
+++ LibraryManagement/Startup.cs
@@ -63,7 +63,7 @@ namespace LibraryManagement
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Library}/{action=Dashboard}/{id?}");
             });
         }
     }
